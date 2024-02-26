077730087a87e2394e508f6306fe0c8b5915e672
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-02-26 16:01:48 +0200

    `try_zip_biased`

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,6 @@
-pub use crate::latest_ready::LatestReadyStreamExt;
-pub use crate::latest_ready::TryLatestReadyStreamExt;
 pub use crate::expand::ExpandStreamExt;
 pub use crate::expand::TryExpandStreamExt;
+pub use crate::latest_ready::LatestReadyStreamExt;
+pub use crate::latest_ready::TryLatestReadyStreamExt;
+pub use crate::zip_biased::TryZipBiasedStreamExt;
 pub use crate::zip_biased::ZipBiasedStreamExt;
--- src/zip_biased.rs
+++ src/zip_biased.rs
@@ -3,7 +3,7 @@ use std::{
     task::{Context, Poll},
 };
 
-use futures::Stream;
+use futures::{Stream, TryStream};
 
 pub trait ZipBiasedStreamExt: Stream + Sized {
     fn zip_biased<R>(self, right: R) -> ZipBiased<Self, R, Self::Item>
@@ -11,6 +11,15 @@ pub trait ZipBiasedStreamExt: Stream + Sized {
         R: Stream,
     {
         ZipBiased::new(self, right)
+    }
+}
+
+pub trait TryZipBiasedStreamExt: Stream + TryStream + Sized {
+    fn try_zip_biased<R>(self, right: R) -> TryZipBiased<Self, R, Self::Ok>
+    where
+        R: Stream + TryStream<Error = Self::Error>,
+    {
+        TryZipBiased::new(self, right)
     }
 }
 
@@ -25,7 +34,28 @@ pub struct ZipBiased<L, R, LI> {
     left_poll: Poll<Option<LI>>,
 }
 
+#[derive(Debug, Clone, Copy)]
+#[pin_project::pin_project]
+pub struct TryZipBiased<L, R, LI> {
+    #[pin]
+    left: L,
+    #[pin]
+    right: R,
+
+    left_poll: Poll<Option<LI>>,
+}
+
 impl<L, R, LI> ZipBiased<L, R, LI> {
+    pub fn new(left: L, right: R) -> Self {
+        Self {
+            left,
+            right,
+            left_poll: Poll::Pending,
+        }
+    }
+}
+
+impl<L, R, LI> TryZipBiased<L, R, LI> {
     pub fn new(left: L, right: R) -> Self {
         Self {
             left,
@@ -49,6 +79,9 @@ where
 
         Poll::Ready(loop {
             match this.left_poll {
+                Poll::Pending => {
+                    *this.left_poll = Poll::Ready(ready!(this.left.as_mut().poll_next(cx)))
+                }
                 Poll::Ready(None) => break None,
                 Poll::Ready(some_left @ Some(_)) => {
                     let right_opt = ready!(this.right.as_mut().poll_next(cx));
@@ -56,8 +89,37 @@ where
                     *this.left_poll = Poll::Pending;
                     break some_left.zip(right_opt);
                 }
-                Poll::Pending => {
-                    *this.left_poll = Poll::Ready(ready!(this.left.as_mut().poll_next(cx)));
+            }
+        })
+    }
+}
+
+impl<L, R> Stream for TryZipBiased<L, R, L::Ok>
+where
+    L: Stream + TryStream,
+    R: Stream + TryStream<Error = L::Error>,
+    L: Stream<Item = Result<L::Ok, L::Error>>,
+    R: Stream<Item = Result<R::Ok, L::Error>>,
+{
+    type Item = Result<(L::Ok, R::Ok), L::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        use std::task::ready;
+
+        let mut this = self.project();
+
+        Poll::Ready(loop {
+            match this.left_poll {
+                Poll::Pending => match ready!(this.left.as_mut().poll_next(cx)).transpose() {
+                    Err(reason) => break Some(Err(reason)),
+                    Ok(left_opt) => *this.left_poll = Poll::Ready(left_opt),
+                },
+                Poll::Ready(None) => break None,
+                Poll::Ready(some_left @ Some(_)) => {
+                    match ready!(this.right.as_mut().poll_next(cx)).transpose() {
+                        Err(reason) => break Some(Err(reason)),
+                        Ok(right_opt) => break some_left.take().zip(right_opt).map(Ok),
+                    }
                 }
             }
         })
@@ -65,3 +127,28 @@ where
 }
 
 impl<L> ZipBiasedStreamExt for L where L: Stream + Sized {}
+impl<L> TryZipBiasedStreamExt for L where L: Stream + TryStream + Sized {}
+
+#[cfg(test)]
+mod tests {
+    use futures::stream;
+    use futures::StreamExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn left_empty() {
+        let left = stream::empty::<()>();
+        let right = stream::repeat(());
+
+        assert_eq!(left.zip_biased(right).collect::<Vec<_>>().await, vec![]);
+    }
+
+    #[tokio::test]
+    async fn right_empty() {
+        let left = stream::repeat(());
+        let right = stream::empty::<()>();
+
+        assert_eq!(left.zip_biased(right).collect::<Vec<_>>().await, vec![]);
+    }
+}
